12ead8ed8045dd91f2420a184d61cffde4dce762
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-01-27 20:54:06 -0500

    Update products implemented for admins

--- src/Routes.js
+++ src/Routes.js
@@ -15,6 +15,8 @@ import Product from "./core/Product";
 import Cart from "./core/Cart";
 import Orders from "./admin/Orders";
 import Profile from './user/Profile';
+import ManageProducts from "./admin/ManageProducts";
+import UpdateProduct from "./admin/UpdateProduct";
 
 const Routes = () => {
     return (
@@ -25,15 +27,18 @@ const Routes = () => {
                 <Route path='/shop' exact component={Shop}></Route>
                 <Route path='/cart' exact component={Cart}></Route>
                 <Route path='/product/:productId' exact component={Product}></Route>
-
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
+
                 <PrivateRoute path="/profile/:userId" exact component={Profile} />
                 <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+
                 <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
                 <AdminRoute path="/create/category" exact component={AddCategory}></AdminRoute>
                 <AdminRoute path="/create/product" exact component={AddProduct}></AdminRoute>
                 <AdminRoute path="/admin/orders" exact component={Orders}></AdminRoute>
+                <AdminRoute path="/admin/products" exact component={ManageProducts}></AdminRoute>
+                <AdminRoute path="/admin/product/update/:productId" exact component={UpdateProduct}></AdminRoute>
             </Switch>
         </BrowserRouter >
     )
